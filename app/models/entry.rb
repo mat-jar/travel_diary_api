edfe8608f0c3bf7ba3ca9eacb70daeb21c9f012f
mat-jar <mtu07@poczta.fm>
2022-12-18 22:58:45 +0100

    Add slug to Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,12 @@
 class Entry < ApplicationRecord
+  validates :title, :note, :place, :weather, presence: true
+
+  def slug
+    return nil unless persisted?
+    "#{id}-#{title.parameterize}" # 12-human-body
+  end
+
+  def as_json(options={})
+    super(:methods => ['slug'])
+  end
 end
