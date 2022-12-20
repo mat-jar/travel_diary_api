5957a5122b4629e89a7b3a8f8523338b0ea6bd82
mat-jar <mtu07@poczta.fm>
2022-12-20 02:38:32 +0100

    Add status to ActiveRecord exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
 
   wrap_parameters false
+
+  rescue_from ActiveRecord::RecordNotFound do |_exception|
+    render status: :not_found #404
+  end
 end
